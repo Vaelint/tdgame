373241a8f8ea559e57a64a005cd69d0400590e25
Frey <freylint@tuta.io>
2021-01-04 09:03:11 -0600

    Remove unused components.

--- src/ecs/simple.rs
+++ src/ecs/simple.rs
@@ -11,28 +11,13 @@ pub struct SimpleComponentsPlug;
 impl Plugin for SimpleComponentsPlug {
     fn build(&self, app: &mut AppBuilder) {
         // Register types for reflection
-        app.register_type::<Accumulator>()
-            .register_type::<DebugSwitch>()
-            .register_type::<DebugSwitch>()
+        app
             .register_type::<Rotating>();
 
         // Register component systems
         app.add_system(Rotating::rotate_sys.system());
     }
 }
-
-/// Accumulator component
-#[derive(Debug, Display, From, Into, Constructor, Reflect, Default)]
-#[reflect(Component)]
-pub struct Accumulator(pub isize);
-
-/// Debug switch component
-#[derive(Debug, Display, From, Into, Constructor, Reflect)]
-pub struct DebugSwitch(pub bool);
-
-/// Entity enabling/disabling component
-#[derive(Debug, Display, From, Into, Constructor, Reflect)]
-pub struct Active(pub bool);
 
 /// Entity enabling/disabling component
 #[derive(Debug, Display, From, Into, Constructor, Reflect)]
