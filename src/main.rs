873fadc87e284c5b31ee9f9a05d34ff83949ff4c
Frey <freylint@tuta.io>
2021-01-04 08:52:44 -0600

    Stop enforcing usage of clippy.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 //! Prototype tower defense game written in bevy
 
-// TODO setup build system to run clippy without cfg
-#![warn(clippy::all)]
-
 use bevy::prelude::*;
 
 use ecs::ProjectECSPlugins;
