7163e133811a1a9e1d27b7a6447404c354fa8ff7
Frey <freylint@tuta.io>
2021-01-08 14:31:53 -0600

    Apply patch suggested by Joey from the bevy discord.

--- src/state/loading_state.rs
+++ src/state/loading_state.rs
@@ -33,20 +33,26 @@ impl LoadState {
     fn setup_world(com: &mut Commands) {
         // Spawn 2D camera
         com.spawn(Camera2dBundle {
+            /*
             camera: Default::default(),
             orthographic_projection: Default::default(),
             visible_entities: Default::default(),
             transform: Default::default(),
             global_transform: Default::default(),
+             */
+            ..Default::default()
         });
 
         // Spawn UI camera
         com.spawn(CameraUiBundle {
+            /*
             camera: Default::default(),
             orthographic_projection: Default::default(),
             visible_entities: Default::default(),
             transform: Default::default(),
             global_transform: Default::default(),
+             */
+            ..Default::default()
         });
     }
 
